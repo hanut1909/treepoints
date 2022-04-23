e8f7b5afd6d78e65184e3eb6758612a51bcf1ea3
Hanut Singh <70129367+hanut1909@users.noreply.github.com>
2022-04-22 18:36:40 -0700

    Create Point.java

---
+++ Point.java
@@ -0,0 +1,33 @@
+package dbscan;
+
+public class Point 
+{
+	private int x;
+
+	private int y;
+
+	
+	Point(int a, int b)
+	 {
+		x=a;
+		y=b;
+	 }
+	
+	public int getX ()
+	 {
+
+		return x;
+
+	 }
+
+
+	public int getY () 
+	{
+
+		return y;
+
+	}
+
+	
+
+}
